6fb7cbabe2f2c316852bc0f141daabc0a86c5de9
Yanwen Huangxu <huangxu.y@husky.neu.edu>
2019-02-04 11:03:50 -0800

    add initial files

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+
+app.get("/", function(req, res){
+	console.log("Request on /");
+	res.send("helloworld");
+})
+
+app.listen(3000, function(){
+	console.log("Server listening for connectins on port 3000");
+})
